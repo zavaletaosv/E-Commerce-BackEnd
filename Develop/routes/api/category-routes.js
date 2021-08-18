18991c4107ad9d3daae7680d0e3b60363a0e99a3
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-18 18:44:55 -0500

    api/product-routes.js updated: product routes created

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No location found with this id!' });
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
 
@@ -63,7 +63,7 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No location found with this id!' });
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
 
