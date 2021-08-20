a5adbf886450526361a1ac2ab7245bf8850f3fb4
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-19 19:11:03 -0500

    models/server.js updated: belongToMany changed to belongsToMany

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -11,10 +11,10 @@ Product.belongsTo(Category);
 Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, { through: 'ProductTag' });
+Product.belongsToMany(Tag, { through: 'ProductTag' });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, { through: 'ProductTag' });
+Tag.belongsToMany(Product, { through: 'ProductTag' });
 
 module.exports = {
   Product,
