07dd08c059f2afa6d46fe5df23a7d3b516768f9c
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-19 21:38:58 -0500

    Develop folder deleted, sub folders moved

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const routes = require('./Develop/routes');
-const sequelize = require('./Develop/config/connection');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
